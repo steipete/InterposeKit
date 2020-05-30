e9b61744c94b4766afc1f5a85bcf97521cb6daa0
Peter Steinberger <steipete@gmail.com>
2020-05-30 22:53:13 +0200

    Add supported platforms

--- Package.swift
+++ Package.swift
@@ -5,6 +5,12 @@ import PackageDescription
 
 let package = Package(
     name: "InterposeKit",
+    platforms: [
+       .iOS(.v11),
+       .macOS(.v10_13),
+       .tvOS(.v11),
+       .watchOS(.v5)
+     ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
