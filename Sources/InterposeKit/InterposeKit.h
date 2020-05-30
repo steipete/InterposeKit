469ce71583e32383f7dee548a9978e7e24af2283
Peter Steinberger <steipete@gmail.com>
2020-05-31 00:33:31 +0200

    add support for xcodebuild

---
+++ Sources/InterposeKit/InterposeKit.h
@@ -0,0 +1,15 @@
+//
+//  InterposeKit.h
+//  InterposeKit
+//
+//  Created by Peter Steinberger on 07.05.20.
+//  Copyright © 2020 PSPDFKit GmbH. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for InterposeKit.
+FOUNDATION_EXPORT double InterposeKitVersionNumber;
+
+//! Project version string for InterposeKit.
+FOUNDATION_EXPORT const unsigned char InterposeKitVersionString[];
