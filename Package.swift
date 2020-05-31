66c8983814d3523ccb6078b9dba5c20f652ca047
Peter Steinberger <steipete@gmail.com>
2020-05-31 02:39:15 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -20,7 +20,7 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
-            name: "InterposeKit",
+            name: "InterposeKit"),
         .testTarget(
             name: "InterposeKitTests",
             dependencies: ["InterposeKit"]),
