10c4aa44d6d5f44d23c057f6d7e4cbf128df5a70
Peter Steinberger <steipete@gmail.com>
2020-06-09 18:38:33 +0200

    Document!

--- Sources/ITKAddSuperMethod/ITKAddSuperMethod.h
+++ Sources/ITKAddSuperMethod/ITKAddSuperMethod.h
@@ -9,14 +9,58 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
-/**
- Adds an empty super implementation instance method to klass.
- If a method already exists, this will return NO.
+NSString *const SuperBuilderErrorDomain;
 
- @note This uses inline assembly to forward the parameters to objc_msgSendSuper.
- Currently implemented architectures are x86_64 and arm64.
- (arm7 was dropped in OS 11 and i386 with macOS Catalina.)
+typedef NS_ERROR_ENUM(SuperBuilderErrorDomain, SuperBuilderErrorCode) {
+    SuperBuilderErrorCodeNoSuperClass,
+    SuperBuilderErrorCodeNoDynamicallyDispatchedMethodAvailable,
+    SuperBuilderErrorCodeFailedToAddMethod
+};
+
+@interface SuperBuilder : NSObject
+
+/**
+ Adds an empty super implementation instance method to originalClass.
+ If a method already exists, this will return NO and a descriptive error message.
+
+ Example: You have an empty UIViewController subclass and call this with viewDidLoad as selector.
+ The result will be code that looks similar to this:
+
+ override func viewDidLoad() {
+     super.viewDidLoad()
+ }
+
+ What the compiler creates in following code:
+
+ - (void)viewDidLoad {
+     struct objc_super _super = {
+         .receiver = self,
+         .super_class = object_getClass(obj);
+     };
+     objc_msgSendSuper2(&_super, _cmd);
+ }
+
+ There are a few important details:
+
+ 1) We use objc_msgSendSuper2, not objc_msgSendSuper.
+   The difference is minor, but important.
+   objc_msgSendSuper starts looking at the current class, which would cause an endless loop
+   objc_msgSendSuper2 looks for the superclass.
+
+ 2) This uses a completely dynamic lookup.
+   While slightly slower, this is resilient even if you change superclasses later on.
+
+ 3) The resolution method calls out to C, so it could be customized to jump over specific implementations.
+   (Such API is not currently exposed)
+
+ 4) This uses inline assembly to forward the parameters to objc_msgSendSuper2 and objc_msgSendSuper2_stret.
+   This is currently implemented architectures are x86_64 and arm64.
+   armv7 was dropped in OS 11 and i386 with macOS Catalina.
  */
-BOOL IKTAddSuperImplementationToClass(id self, Class klass, SEL selector);
++ (BOOL)addSuperInstanceMethodToClass:(Class)originalClass selector:(SEL)selector error:(NSError **)error;
+
+@end
+
+
 
 NS_ASSUME_NONNULL_END
--- Sources/ITKAddSuperMethod/ITKAddSuperMethod.m
+++ Sources/ITKAddSuperMethod/ITKAddSuperMethod.m
@@ -13,21 +13,89 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
+NSString *const PSPDFErrorDomain = @"com.steipete.superbuilder";
+
 void msgSendSuperTrampoline(void);
 void msgSendSuperStretTrampoline(void);
 
+typedef NS_ENUM(NSInteger, DispatchMode) {
+    DispatchModeNormal,
+    DispatchModeStret,
+};
+
+#define let const __auto_type
+#define var __auto_type
+
+static DispatchMode IKTGetDispatchMode(const char *typeEncoding) {
+    DispatchMode dispatchMode = DispatchModeNormal;
+#if defined (__arm64__)
+    // ARM64 doesn't use stret dispatch. Yay!
+#elif defined (__x86_64__)
+    // On x86-64, stret dispatch is ~used whenever return type doesn't fit into two registers
+    //
+    // http://www.sealiesoftware.com/blog/archive/2008/10/30/objc_explain_objc_msgSend_stret.html
+    // x86_64 is more complicated, including rules for returning floating-point struct fields in FPU registers, and ppc64's rules and exceptions will make your head spin. The gory details are documented in the Mac OS X ABI Guide, though as usual if the documentation and the compiler disagree then the documentation is wrong.
+    NSUInteger returnTypeActualSize = 0;
+    NSGetSizeAndAlignment(typeEncoding, &returnTypeActualSize, NULL);
+    dispatchMode = returnTypeActualSize > (sizeof(void *) * 2) ? DispatchModeStret : DispatchModeNormal;
+#else
+#error - Unknown architecture
+#endif
+    return dispatchMode;
+}
+
+// Helper for binding with Swift
+BOOL IKTAddSuperImplementationToClass(Class originalClass, SEL selector, NSError **error);
+BOOL IKTAddSuperImplementationToClass(Class originalClass, SEL selector, NSError **error) {
+    return [SuperBuilder addSuperInstanceMethodToClass:originalClass selector:selector error:error];
+}
+
+#define ERROR_AND_RETURN(CODE, STRING)\
+if (error) { *error = [NSError errorWithDomain:SuperBuilderErrorDomain code:CODE userInfo:@{NSLocalizedDescriptionKey: STRING}];} return NO;
+
+@implementation SuperBuilder
+
++ (BOOL)addSuperInstanceMethodToClass:(Class)originalClass selector:(SEL)selector error:(NSError **)error {
+    let superClass = class_getSuperclass(originalClass);
+    if (superClass == nil) {
+        let msg = [NSString stringWithFormat:@"Unable to find superclass for %@", NSStringFromClass(originalClass)];
+        ERROR_AND_RETURN(SuperBuilderErrorCodeNoSuperClass, msg)
+    }
+    let method = class_getInstanceMethod(superClass, selector);
+    if (method == NULL) {
+        let msg = [NSString stringWithFormat:@"No dynamically dispatched method with selector %@ is available on any of the superclasses of %@", NSStringFromSelector(selector), NSStringFromClass(originalClass)];
+        ERROR_AND_RETURN(SuperBuilderErrorCodeNoDynamicallyDispatchedMethodAvailable, msg)
+    }
+    const char *typeEncoding = method_getTypeEncoding(method);
+    let isNormalDispatch = IKTGetDispatchMode(typeEncoding) == DispatchModeNormal;
+    IMP trampoline = isNormalDispatch ? msgSendSuperTrampoline : msgSendSuperStretTrampoline;
+    let methodAdded = class_addMethod(originalClass, selector, trampoline, typeEncoding);
+    if (!methodAdded) {
+        let msg = [NSString stringWithFormat:@"Failed to add method for selector %@ to class %@", NSStringFromSelector(selector), NSStringFromClass(originalClass)];
+        ERROR_AND_RETURN(SuperBuilderErrorCodeFailedToAddMethod, msg)
+    }
+    return methodAdded;
+}
+
+@end
+
+// One thread local per thread should be enough
 _Thread_local struct objc_super _threadSuperStorage;
 
 struct objc_super *ITKReturnThreadSuper(__unsafe_unretained id obj);
 struct objc_super *ITKReturnThreadSuper(__unsafe_unretained id obj) {
     struct objc_super *_super = &_threadSuperStorage;
     _super->receiver = obj;
-    _super->super_class = [obj class];
+    _super->super_class = object_getClass(obj);
     return _super;
 }
 
 /**
+ Inline assembly is used to perfectly forward all parameters to objc_msgSendSuper,
+ while also looking up the target on-the-fly.
+
  Assembly is hard, here are some useful resources:
+
  https://azeria-labs.com/functions-and-the-stack-part-7/
  https://github.com/DavidGoldman/InspectiveC/blob/master/InspectiveCarm64.mm
  https://blog.nelhage.com/2010/10/amd64-and-va_arg/
@@ -72,7 +140,7 @@ void msgSendSuperTrampoline(void) {
                   // get new return (adr of the objc_super class)
                   "mov x0, x9\n"
                   // tail call
-                  "b _objc_msgSendSuper \n"
+                  "b _objc_msgSendSuper2 \n"
                   : : : "x0", "x1");
 }
 
@@ -84,36 +152,42 @@ void msgSendSuperStretTrampoline(void) {}
 __attribute__((__naked__))
 void msgSendSuperTrampoline(void) {
     asm volatile (
-                  "pushq   %%rbp                 # push frame pointer \n"
-                  "movq    %%rsp, %%rbp          # set stack to frame pointer \n"
-                  "subq    $48, %%rsp            # reserve 48 byte on the stack (need 16 byte alignment) \n"
+                  //  push frame pointer
+                  "pushq %%rbp \n"
+                  // set stack to frame pointer
+                  "movq %%rsp, %%rbp \n"
+                  // reserve 48 byte on the stack (need 16 byte alignment)
+                  "subq $48, %%rsp \n"
 
                   // Save call params: rdi, rsi, rdx, rcx, r8, r9
-                  "movq    %%rdi, -8(%%rbp)      # copy self to stack[1] \n" // po *(id *)
-                  "movq    %%rsi, -16(%%rbp)     # copy _cmd to stack[2] \n" // p (SEL)$rsi
-                  "movq    %%rdx, -24(%%rbp) \n"
-                  "movq    %%rcx, -32(%%rbp) \n"
-                  "movq    %%r8,  -40(%%rbp) \n"
-                  "movq    %%r9,  -48(%%rbp) \n"
+                  "movq %%rdi, -8(%%rbp)  \n" // self po *(id *)
+                  "movq %%rsi, -16(%%rbp) \n" // _cmd p (SEL)$rsi
+                  "movq %%rdx, -24(%%rbp) \n" // param 1
+                  "movq %%rcx, -32(%%rbp) \n" // param 2
+                  "movq %%r8,  -40(%%rbp) \n" // param 3
+                  "movq %%r9,  -48(%%rbp) \n" // param 4 (rest goes on stack)
 
                   // fetch filled struct objc_super, call with self + _cmd
-                  "callq    _ITKReturnThreadSuper \n"
+                  "callq _ITKReturnThreadSuper \n"
                   // first param is now struct objc_super
                   "movq %%rax, %%rdi \n"
 
                   // Restore call params
-                  "movq    -16(%%rbp), %%rsi \n"
-                  "movq    -24(%%rbp), %%rdx \n"
-                  "movq    -32(%%rbp), %%rcx \n"
-                  "movq    -40(%%rbp), %%r8  \n"
-                  "movq    -48(%%rbp), %%r9  \n"
+                  // do not restore first parameter: super class
+                  "movq -16(%%rbp), %%rsi \n"
+                  "movq -24(%%rbp), %%rdx \n"
+                  "movq -32(%%rbp), %%rcx \n"
+                  "movq -40(%%rbp), %%r8  \n"
+                  "movq -48(%%rbp), %%r9  \n"
 
-                  // remove everything to prepare tail call
                   // debug stack via print  *(int *)  ($rsp+8)
-                  "addq    $48, %%rsp            # remove 64 byte from stack \n"
-                  "popq    %%rbp                 # pop frame pointer \n"
+                  // remove 64 byte from stack
+                  "addq $48, %%rsp \n"
+                  // pop frame pointer
+                  "popq  %%rbp \n"
 
-                  "jmp _objc_msgSendSuper        # tail call \n"
+                  // tail call time!
+                  "jmp _objc_msgSendSuper2 \n"
                   : : : "rsi", "rdi");
 }
 
@@ -121,94 +195,51 @@ void msgSendSuperTrampoline(void) {
 __attribute__((__naked__))
 void msgSendSuperStretTrampoline(void) {
     asm volatile (
-                  "pushq   %%rbp                 # push frame pointer \n"
-                  "movq    %%rsp, %%rbp          # set stack to frame pointer \n"
-                  "subq    $48, %%rsp            # reserve 48 byte on the stack (need 16 byte alignment) \n"
+                  //  push frame pointer
+                  "pushq %%rbp \n"
+                  // set stack to frame pointer
+                  "movq %%rsp, %%rbp \n"
+                  // reserve 48 byte on the stack (need 16 byte alignment)
+                  "subq $48, %%rsp \n"
 
                   // Save call params: rax(for va_arg) rdi, rsi, rdx, rcx, r8, r9
-                  "movq    %%rdi, -8(%%rbp) \n"  // struct return
-                  "movq    %%rsi, -16(%%rbp) \n" // self
-                  "movq    %%rdx, -24(%%rbp) \n" // _cmd
-                  "movq    %%rcx, -32(%%rbp) \n" // param 1
-                  "movq    %%r8,  -40(%%rbp) \n" // param 2
-                  "movq    %%r9,  -48(%%rbp) \n" // param 3
+                  "movq %%rdi, -8(%%rbp) \n"  // struct return
+                  "movq %%rsi, -16(%%rbp) \n" // self
+                  "movq %%rdx, -24(%%rbp) \n" // _cmd
+                  "movq %%rcx, -32(%%rbp) \n" // param 1
+                  "movq %%r8,  -40(%%rbp) \n" // param 2
+                  "movq %%r9,  -48(%%rbp) \n" // param 3 (rest goes on stack)
 
                   // fetch filled struct objc_super, call with self + _cmd
                   // Since stret offsets, we move back by one
-                  "movq     -16(%%rbp), %%rdi \n"
-                  "movq     -24(%%rbp), %%rdx \n"
-                  "callq    _ITKReturnThreadSuper \n"
+                  "movq -16(%%rbp), %%rdi \n"
+                  "movq -24(%%rbp), %%rdx \n"
+                  "callq _ITKReturnThreadSuper \n"
                   // second param is now struct objc_super
                   "movq %%rax, %%rsi \n"
                   // First is our struct return
 
                   // Restore call params
-                  "movq     -8(%%rbp), %%rdi \n"
-                  //"movq    -16(%%rbp), %%rsi \n"
-                  "movq    -24(%%rbp), %%rdx \n"
-                  "movq    -32(%%rbp), %%rcx \n"
-                  "movq    -40(%%rbp), %%r8  \n"
-                  "movq    -48(%%rbp), %%r9  \n"
+                  "movq -8(%%rbp), %%rdi \n"
+                  // do not restore second parameter: super class
+                  "movq -24(%%rbp), %%rdx \n"
+                  "movq -32(%%rbp), %%rcx \n"
+                  "movq -40(%%rbp), %%r8  \n"
+                  "movq -48(%%rbp), %%r9  \n"
 
-                  // remove everything to prepare tail call
                   // debug stack via print  *(int *)  ($rsp+8)
-                  "addq    $48, %%rsp            # remove 64 byte from stack \n"
-                  "popq    %%rbp                 # pop frame pointer \n"
+                  // remove 64 byte from stack
+                  "addq $48, %%rsp \n"
+                  // pop frame pointer
+                  "popq  %%rbp \n"
 
-                  "jmp _objc_msgSendSuper_stret   # tail call \n"
+                  // tail call time!
+                  "jmp _objc_msgSendSuper2_stret  \n"
                   : : : "rsi", "rdi");
 }
 
-#endif
-
-typedef NS_ENUM(NSInteger, DispatchMode) {
-    DispatchModeNormal,
-    DispatchModeStret,
-};
-
-static DispatchMode IKTGetDispatchMode(const char *typeEncoding) {
-    DispatchMode dispatchMode = DispatchModeNormal;
-#if defined (__arm64__)
-    // ARM64 doesn't use stret dispatch. Yay!
-#elif defined (__x86_64__)
-    // On x86-64, stret dispatch is ~used whenever return type doesn't fit into two registers
-    //
-    // http://www.sealiesoftware.com/blog/archive/2008/10/30/objc_explain_objc_msgSend_stret.html
-    // x86_64 is more complicated, including rules for returning floating-point struct fields in FPU registers, and ppc64's rules and exceptions will make your head spin. The gory details are documented in the Mac OS X ABI Guide, though as usual if the documentation and the compiler disagree then the documentation is wrong.
-    NSUInteger returnTypeActualSize = 0;
-    NSGetSizeAndAlignment(typeEncoding, &returnTypeActualSize, NULL);
-    dispatchMode = returnTypeActualSize > (sizeof(void *) * 2) ? DispatchModeStret : DispatchModeNormal;
 #else
-#error - Unknown architecture
+#error - Unknown architecture - time to write some assembly :)
 #endif
-    return dispatchMode;
-}
-
-BOOL IKTAddSuperImplementationToClass(id self, Class klass, SEL selector) {
-    Class originalClass = klass;
-
-    Class superClass = class_getSuperclass(originalClass);
-    if (superClass == nil) {
-        return NO;
-    }
-    Method method = class_getInstanceMethod(superClass, selector);
-    if (method == NULL) {
-        [NSException raise:NSInternalInconsistencyException
-                    format:@"No dynamically dispatched method with selector %@ is available on any of the superclasses of %@",
-         NSStringFromSelector(selector), NSStringFromClass(originalClass)];
-        return NO;
-    }
-    const char *typeEncoding = method_getTypeEncoding(method);
-    const BOOL isNormalDispatch = IKTGetDispatchMode(typeEncoding) == DispatchModeNormal;
-    IMP trampoline = isNormalDispatch ? msgSendSuperTrampoline : msgSendSuperStretTrampoline;
-    BOOL methodAdded = class_addMethod(klass, selector, trampoline, typeEncoding);
-    if (!methodAdded) {
-        NSLog(@"Failed to add method for selector %@ to class %@",
-              NSStringFromSelector(selector),
-              NSStringFromClass(klass));
-    }
-
-    return methodAdded;
-}
 
 NS_ASSUME_NONNULL_END
